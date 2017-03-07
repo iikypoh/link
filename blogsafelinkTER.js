6f61d99b53b397c4e4d922ec90cf06b780e6cf21
iikypoh <muhtarmizi097@gmail.com>
2017-03-07 15:42:06 +0800

    update safelink

--- blogsafelinkTER.js
+++ blogsafelinkTER.js
@@ -58,7 +58,7 @@ if(a_to_vh==false)
 {var
 
  
-encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://link-dow.blogspot.com/2017/03/get.html?url="+
+encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://link-dow.blogspot.co.id/2017/03/get.html?url="+
 
  encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ 
 a_to_ve[i].href+"\n";}}
